70967da56c8c1626a17f5def6b2fece684e064fc
foreverman <lzm@zhuopucapital.com>
2015-04-21 09:17:02 +0800

    restrict api access through oauth and scope resources under current user

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,19 +1,25 @@
 module Api
   module V1
     class TasksController < ApplicationController
+      # doorkeeper_for :all
+      before_action :doorkeeper_authorize!
 
       def index
-        render json: Task.all
+        render json: current_user.tasks
       end
 
       def create
-        task = Task.create(task_params)
+        task = current_user.tasks.create(task_params)
         render json: task
       end
       
       private
       def task_params
         params.require(:task).permit(:name)
+      end
+
+      def current_user
+        @current_user ||= User.find(doorkeeper_token.resource_owner_id)
       end
     end
   end
